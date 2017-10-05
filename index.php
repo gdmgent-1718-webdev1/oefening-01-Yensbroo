6aff8559c713b396a38bdddacf0587ff518a278f
Yensbroo <yens.broothaers@student.arteveldehs.be>
2017-10-05 10:39:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$naam = "Yens";
+
+echo "<h1> Welkom " . $naam . "</h1>";
+?>
+</body>
+</html>
